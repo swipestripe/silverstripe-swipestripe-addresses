fe343e84183a849109641f622ab3de19480c90ae
Frank Mullenger <frank@silverstripe.com>
2013-08-14 18:39:16 +1200

    MINOR: Change config setup.

--- _config.php
+++ _config.php
@@ -1,6 +1,1 @@
 <?php
-
-Object::add_extension('Order', 'Addresses_Order');
-Object::add_extension('Customer', 'Addresses_Customer');
-Object::add_extension('OrderForm', 'Addresses_OrderForm');
-Object::add_extension('ShopConfig', 'Addresses_Extension');
